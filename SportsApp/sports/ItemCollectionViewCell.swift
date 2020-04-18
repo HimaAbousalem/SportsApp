fb7959eb508760415bef2e3d6abfc6635027f34f
HimaAbousalem <hima.abousalem@gmail.com>
2020-04-18 21:07:54 +0200

    collectionViewCell

---
+++ SportsApp/sports/ItemCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ItemCollectionViewCell.swift
+//  SportsApp
+//
+//  Created by Esraa Hassan on 4/18/20.
+//  Copyright © 2020 eman kamal. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var sportImage: UIImageView!
+    @IBOutlet weak var sportName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
