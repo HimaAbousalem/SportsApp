954ee3d8fdaea441a39bcac0b1f6059bde71b862
eman kamal <emankamal@emans-Mac.local>
2020-04-22 05:26:15 -0700

    team details

---
+++ SportsApp/Features/teamDetails/TeamDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TeamDetailsViewController.swift
+//  SportsApp
+//
+//  Created by eman kamal on 4/21/20.
+//  Copyright © 2020 eman kamal. All rights reserved.
+//
+
+import UIKit
+
+class TeamDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
